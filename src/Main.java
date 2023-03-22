f26723b81e202bd94af2571e0dafd658358c1d5a
Minh Hai <domohai12@gmail.com>
2023-03-22 14:40:21 +0700

    sfskjf

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,35 +6,29 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 6969)) {
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
+        try (Socket socket = new Socket("localhost", 6969);
+            Scanner scanner = new Scanner(System.in)) {
+            // read the result received from server
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // send the data to server
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-            Scanner scanner = new Scanner(System.in);
-            String echoString;
-            String response;
-
+            String inputString, response;
             do {
-                System.out.println("Enter string to be echoed: ");
-                //Read string from clients
-                echoString = scanner.nextLine();
-                // Send string to server
-
-                output.println(echoString);
-                // Check exit or not
-                if (!echoString.equals("exit")) {
-                    // Get data from Input
+                System.out.println("Enter string to be normalized: ");
+                // read string from client via console
+                inputString = scanner.nextLine();
+                // send string to server
+                output.println(inputString);
+                // check whether client wants to exit
+                if (!inputString.equals("quit")) {
+                    // get data received from server
                     response = input.readLine();
-                    System.out.println(response);
+                    System.out.println("Result from server: " + response);
                 }
-            } while (!echoString.equals("exit"));
-
+            } while (!inputString.equals("quit"));
         } catch (IOException e) {
             System.out.println("Client Error: " + e.getMessage());
-
         }
     }
 }
